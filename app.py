dfc63157b991e7afa7550b8d890011055ff458c1
Akhmad Raihan Aulia Fikri <114968073+Akmdrhannn@users.noreply.github.com>
2024-10-21 04:13:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,10 +57,10 @@ def main():
     st.markdown(
         """
         <style>
-        .stApp {
-            background-image: url('https://raw.githubusercontent.com/Akmdrhannn/tesStreamlit/main/bg.jpg');
-            background-size: cover;
-        }
+        # .stApp {
+        #     background-image: url('https://raw.githubusercontent.com/Akmdrhannn/tesStreamlit/main/bg.jpg');
+        #     background-size: cover;
+        # }
         .center-image {
             display: flex;
             justify-content: center;
@@ -79,7 +79,7 @@ def main():
     st.markdown('<div class="justified-text"> Conditional Generative Adversarial Networks (CGAN) adalah metode yang menjadikan sebuah masukan atau input-nya seperti sebuah kondisi. Merujuk pada Conditional, kondisi tersebut dapat digunakan terhadap generator atau discriminator sesuai kebutuhan pengguna sehingga CGAN dapat menghasilkan data buatan yang diinginkan menggunakan kondisi yang spesifik atau telah ditentukan sebelumnya. Pada penelitian ini, kondisi tersebut ialah citra grayscale yang menjadi syarat dalam pelatihan model untuk menghasilkan pewarnaan.</div>', unsafe_allow_html=True)
 
     # Tampilkan gambar di tengah
-    # st.markdown('<div class="center-image"><img src="https://raw.githubusercontent.com/Akmdrhannn/tesStreamlit/main/gambaranUmum.png" width="500"></div>', unsafe_allow_html=True)
+    st.markdown('<div class="center-image"><img src="https://raw.githubusercontent.com/Akmdrhannn/tesStreamlit/main/gambaranUmum.png" width="500"></div>', unsafe_allow_html=True)
     
     st.write("Silahkan upload citra atau gambar grayscale yang akan diwarnai oleh model CGAN.")
     
